4d7a4829fb2a78ed818b803302bb177076108073
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-03-16 22:27:54 +0530

    final update to connect mongodb

---
+++ src/index.js
@@ -0,0 +1,75 @@
+//require("dotenv").config({path:'./.env'})
+
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+*/
+
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})  // Load environment variables from .env file
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//IMPORTANT NOTE 
+
+//in package .json 
+    /* we can also use the below line as well it is also working fine */
+    // "dev": "nodemon src/index.js"
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+
+    }
+    catch(error){
+        console.log("Error: ", error)
+        throw error
+    }
+})()
+
+*/
