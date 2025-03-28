576ef29752908db6b6cbd2ba652594777205d152
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-03-29 00:29:48 +0530

    updating

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,11 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Hello! Sai Smruti Ranjan Das"
-    })
+
+
+
+    const{fullName, email, username, passsword} = req.body
+    console.log("email", email)
     
 })
 
