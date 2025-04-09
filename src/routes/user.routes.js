6b882e968d0b98d623703dca244858a6d40e87d2
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-04-09 23:37:21 +0530

    Updating the files

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import {  registerUser } from "../controllers/user.controller.js";
+
+import { 
+    loginUser, 
+    logoutUser, 
+} from "../controllers/user.controller.js";
+
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 
