8ae8c7e138cf22eca996e5c22a360a6c7985d251
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-06-12 00:38:40 +0530

    update code part

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -257,9 +257,47 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 })
 
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true}
+        
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+});
+
+
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser
 }
