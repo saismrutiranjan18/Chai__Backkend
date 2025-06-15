9dc0e3a300c8ec7acea1edbe1b80fbfae8c2a52f
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-06-15 10:30:33 +0530

    update router.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,20 @@
 import { Router } from "express";
-import {  
-    registerUser,
+import { 
     loginUser, 
     logoutUser, 
+    registerUser, 
     refreshAccessToken, 
- } from "../controllers/user.controller.js";
-
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
 
 
 const router = Router()
